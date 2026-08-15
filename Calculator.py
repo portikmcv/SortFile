ffa9b2183eb3921b95268694683e06d657d2b4a9
port <portt2552@gmail.com>
2026-08-15 20:51:34 +0700

    Add Input Secure and Cleaner Code

--- Calculator.py
+++ Calculator.py
@@ -2,37 +2,43 @@ import re
 def add(a,b):
     return a + b
 
-def subtract(a,b):
-    return a - b
+def subtract(a,b):return a - b
 
-def multiply(a,b):
-    return a*b
+def multiply(a,b):return a*b
+    
 
-def divide (a,b):
-    if b != 0:
-        return a/b
-    else:
-        return "B cannot be 0"
+def divide (a,b):return a/b if b != 0 else "Error"
+    
 
 
 while True:
     equation = input("Your equation: (Example: 2 * 3)")
     Rawpart = re.split(r'([\+\-\*\/])',equation)
     parts = [p.strip() for p in Rawpart if p.strip()]
-    op = str(parts[1])
-    num1 = float(parts[0])
-    num2 = float(parts[2])
+
+    if len(parts) != 3:
+        print("Invalid format! Please input like '2 * 3'")
+    try:
+        op = str(parts[1])
+        num1 = float(parts[0])
+        num2 = float(parts[2])
+    except ValueError:
+        print("Error: Please enter valid numbers.")
+
+    result = None
     if op == '+':
         result = add(num1,num2)
-        print(f'Your result = {result}')
+        
     elif op == '-':
         result = subtract(num1,num2)
-        print(f'Your result = {result}')
+        
     elif op == '*':
         result = multiply(num1,num2)
-        print(f'Your result = {result}')
+        
     elif op == '/':
         result = divide(num1,num2)
-        print(f'Your result = {result}')
+
+    print(f'Your result = {result}\n')
+        
 
 
