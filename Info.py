fdd6acfcadf4731aac9f38477fdcaf81da1bb7e7
port <portt2552@gmail.com>
2026-08-16 11:52:04 +0700

    Finish

--- Info.py
+++ Info.py
@@ -24,17 +24,55 @@ def savedb(dbname,metadata):
                 VALUES (:filename,:filepath,:resolution,:fps,:duration)
             """,metadata)
 
-def scan():
-    pass
+def scan(path):
+    cap = cv.VideoCapture(str(path))
+    if not cap.isOpened():
+         return None
+
+    width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv.CAP_PROP_FPS)
+    framcount = cap.get(cv.CAP_PROP_FRAME_COUNT)
+
+    duration = framcount / fps
+    if fps <= 0:
+        cap.release()
+        return None
+    else:
+         cap.release()
+
+    return {'filename':path.name,
+            'filepath':str(path),
+            'resolution':f'{width}x{height}',
+            'fps':round(fps,2),
+            'duration':round(duration,2)}
+
+            
+
+    
 
 
 def main(path,dbname):
     setupdb(dbname)
+
+    if not path.exists():
+         print("please input a valid path")
+         return
+
+    for file in path.iterdir():
+         if file.suffix.lower() in [".mov",".mp4"]:
+              metadata = scan(file)
+              if metadata:
+                   savedb(dbname,metadata)
+
+
+    
     
 
 
 
 
-
-path = ''
-main(path,dbname = 'db1')
+if __name__ == "__main__":
+    path = r'C:\Users\User\Videos\Roblox'
+    path = Path(path)
+    main(path,dbname = 'db1')
