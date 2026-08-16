7633a204a71755f55602ad2a57becf389ecc61ee
port <portt2552@gmail.com>
2026-08-16 09:52:26 +0700

    Add DB and SAVETODB function

--- Info.py
+++ Info.py
@@ -3,18 +3,33 @@ import sqlite3
 from pathlib import Path
 
 
-def setupdb():
-    pass
+def setupdb(dbname):
+    with sqlite3.connect(dbname) as conn:
+        cur = conn.cursor()
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS videos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            filename TEXT,
+            filepath TEXT UNIQUE,
+            resolution TEXT,
+            fps REAL,
+            duration REAL)
+        """)
 
-def savedb():
-    pass
+def savedb(dbname,metadata):
+    with sqlite3.connect(dbname) as conn:
+            cur = conn.cursor()
+            cur.execute("""
+            INSERT OR IGNORE INTO videos (filename,filepath,resolution,fps,duration)
+                VALUES (:filename,:filepath,:resolution,:fps,:duration)
+            """,metadata)
 
 def scan():
     pass
 
 
 def main(path,dbname):
-    setupdb()
+    setupdb(dbname)
     
 
 
