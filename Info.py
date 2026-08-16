bf5e878641beafeb65b86d0ea62045aa3e91d9dd
port <portt2552@gmail.com>
2026-08-16 09:42:38 +0700

    Create files and write some strucutre

---
+++ Info.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import sqlite3
+from pathlib import Path
+
+
+def setupdb():
+    pass
+
+def savedb():
+    pass
+
+def scan():
+    pass
+
+
+def main(path,dbname):
+    setupdb()
+    
+
+
+
+
+
+path = ''
+main(path,dbname = 'db1')
